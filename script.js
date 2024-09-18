d4880c26d72ee9c748ee0a48cd932ec60ac423e9
Utibe Essien <utidon23@gmail.com>
2024-09-18 14:07:06 +0100

    just testing

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const header = document.getElementById('header');
 
 setInterval(() => {
     header.classList.toggle('hidden'); // Toggle the 'hidden' class
-}, 500); // Adjust the time (in milliseconds) for faster or slower flashing
+}, 750); // Adjust the time (in milliseconds) for faster or slower flashing
 
